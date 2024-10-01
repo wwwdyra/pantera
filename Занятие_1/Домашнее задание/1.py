7b14558263d4d3e00b675f4dd4adbd0211993b91
wwwdyra <kaverznw@gmail.com>
2024-10-01 11:32:35 +0300

    Create 1.py

---
+++ Занятие_1/Домашнее задание/1.py
@@ -0,0 +1,6 @@
+import math 
+r=int(input('Введите R:'))
+p=round(2*math.pi*r,2)
+s= round(math.pi*r**2,2)
+print('Длина окружности с радиусом',r,'см равна',p)
+print('Площадь окружности с радиусом',r,'см равна',s)
